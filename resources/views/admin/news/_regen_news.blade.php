9c30c8c5a772933f2cafc5d294e1c2f15897c0db
SpeedyD <SpeedyD@users.noreply.github.com>
2024-12-29 16:51:42 +0000

    refactor: fix blade formatting

--- resources/views/admin/news/_regen_news.blade.php
+++ resources/views/admin/news/_regen_news.blade.php
@@ -1,7 +1,8 @@
 @if ($news)
     {!! Form::open(['url' => 'admin/news/regen/' . $news->id]) !!}
 
-    <p>You are about to regenerate the news post <strong>{{ $news->title }}</strong>. This will recreate the post contents based on the currently saved data, which is useful for regenerating mention links. If you've made changes on the news post since clicking the edit button last, these changes will be lost.
+    <p>You are about to regenerate the news post <strong>{{ $news->title }}</strong>. This will recreate the post contents based on the currently saved data, which is useful for regenerating mention links. If you've made changes on the news post
+        since clicking the edit button last, these changes will be lost.
     </p>
     <p>Are you sure you want to regenerate <strong>{{ $news->title }}</strong>?</p>
 
