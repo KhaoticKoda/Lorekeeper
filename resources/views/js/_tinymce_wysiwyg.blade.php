1f3c542efa74aceac202edc953c1b30c0dd7ceab
SpeedyD <speedydvv@gmail.com>
2025-01-27 00:01:17 +0100

    Edit Code Editor option to extensions.php

--- resources/views/js/_tinymce_wysiwyg.blade.php
+++ resources/views/js/_tinymce_wysiwyg.blade.php
@@ -6,9 +6,9 @@ tinymce.init({
     plugins: [
         'advlist autolink lists link image charmap print preview anchor',
         'searchreplace visualblocks fullscreen spoiler',
-        'insertdatetime media table paste codeeditor help wordcount'
+        'insertdatetime media table paste {{ (config('lorekeeper.extensions.tinymce_code_editor')) ? 'codeeditor' : 'code' }} help wordcount'
     ],
-    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | codeeditor',
+    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | spoiler-add spoiler-remove | removeformat | {{ (config('lorekeeper.extensions.tinymce_code_editor')) ? 'codeeditor' : 'code' }}',
     content_css: [
         '{{ asset('css/app.css') }}',
         '{{ asset('css/lorekeeper.css') }}'
