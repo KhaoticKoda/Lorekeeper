c943d5157cffefb8634481d82b0102fbf375d27a
ScuffedNewt <dragonminer101@gmail.com>
2025-01-28 21:29:39 +0000

    fix: reply modal sub menus

--- resources/views/js/_modal_wysiwyg.blade.php
+++ resources/views/js/_modal_wysiwyg.blade.php
@@ -1,16 +1,26 @@
-tinymce.init({
-selector: '#modal .wysiwyg',
-height: 500,
-menubar: false,
-plugins: [
-'advlist autolink lists link image charmap print preview anchor',
-'searchreplace visualblocks fullscreen',
-'insertdatetime media table paste codeeditor help wordcount'
-],
-toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | codeeditor',
-content_css: [
-'//www.tiny.cloud/css/codepen.min.css',
-'{{ asset('css/app.css') }}',
-'{{ asset('css/lorekeeper.css') }}'
-]
-});
+<script>
+    $(document).on('focusin', function(e) {
+    if ($(e.target).closest(".tox-tinymce, .tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
+        e.stopImmediatePropagation();
+    }
+    });
+
+    $(document).ready(function() {
+        tinymce.init({
+            selector: '#modal .wysiwyg',
+            height: 500,
+            menubar: false,
+            plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks fullscreen',
+            'insertdatetime media table paste codeeditor help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | codeeditor',
+            content_css: [
+            '//www.tiny.cloud/css/codepen.min.css',
+            '{{ asset('css/app.css') }}',
+            '{{ asset('css/lorekeeper.css') }}'
+            ]
+        });
+    });
+</script>
