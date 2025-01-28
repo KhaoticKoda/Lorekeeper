7411410f7429e700d5be89642a62a6570add9db9
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2025-01-28 21:30:33 +0000

    refactor: fix blade formatting

--- resources/views/js/_modal_wysiwyg.blade.php
+++ resources/views/js/_modal_wysiwyg.blade.php
@@ -1,8 +1,8 @@
 <script>
     $(document).on('focusin', function(e) {
-    if ($(e.target).closest(".tox-tinymce, .tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
-        e.stopImmediatePropagation();
-    }
+        if ($(e.target).closest(".tox-tinymce, .tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
+            e.stopImmediatePropagation();
+        }
     });
 
     $(document).ready(function() {
@@ -11,15 +11,15 @@
             height: 500,
             menubar: false,
             plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks fullscreen',
-            'insertdatetime media table paste codeeditor help wordcount'
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks fullscreen',
+                'insertdatetime media table paste codeeditor help wordcount'
             ],
             toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | codeeditor',
             content_css: [
-            '//www.tiny.cloud/css/codepen.min.css',
-            '{{ asset('css/app.css') }}',
-            '{{ asset('css/lorekeeper.css') }}'
+                '//www.tiny.cloud/css/codepen.min.css',
+                '{{ asset('css/app.css') }}',
+                '{{ asset('css/lorekeeper.css') }}'
             ]
         });
     });
