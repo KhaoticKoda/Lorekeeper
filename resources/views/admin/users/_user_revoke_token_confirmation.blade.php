69b298794282da219d14f54857cff4bb0b82e74b
perappu <perappu@users.noreply.github.com>
2025-02-02 16:33:21 +0000

    refactor: fix blade formatting

--- resources/views/admin/users/_user_revoke_token_confirmation.blade.php
+++ resources/views/admin/users/_user_revoke_token_confirmation.blade.php
@@ -1,4 +1,4 @@
 <p>Are you sure you want to revoke all of {!! $user->displayName !!}'s API tokens?</p>
 {!! Form::open(['url' => 'admin/users/' . $user->name . '/revoke']) !!}
-    {!! Form::submit('Revoke Tokens', ['class' => 'btn btn-danger']) !!}
+{!! Form::submit('Revoke Tokens', ['class' => 'btn btn-danger']) !!}
 {!! Form::close() !!}
