f2e0f07c8adf402a307d0e1c6ba04c3e09833578
notecardPasta <notecardPasta@users.noreply.github.com>
2025-02-10 01:06:42 +0000

    refactor: fix blade formatting

--- resources/views/pages/feeds.blade.php
+++ resources/views/pages/feeds.blade.php
@@ -21,18 +21,17 @@
     <h4 class="mb-0"><a href="{{ url('feeds/sales') }}"><i class="fas fa-rss-square"></i> Sales Feed</a> <i data-toggle="tooltip" title="Click to Copy the URL" data-copy="{{ url('feeds/sales') }}" class="far fa-copy text-small"></i></h4>
     <p class="mb-2">Updates with recent Sales posts on this site.</p>
     <p class="mb-2"><code>{{ url('feeds/sales') }}</code></p>
-
-@endsection 
-@section('scripts') 
-<script>
-    $('.fa-copy').on('click', async (e) => {
-		await window.navigator.clipboard.writeText(e.currentTarget.dataset.copy);
-        e.currentTarget.classList.remove('toCopy');
-        e.currentTarget.classList.add('toCheck');
-        setTimeout(() => {
-            e.currentTarget.classList.remove('toCheck');
-            e.currentTarget.classList.add('toCopy');
-        }, 2000);
-    });
-</script>
+@endsection
+@section('scripts')
+    <script>
+        $('.fa-copy').on('click', async (e) => {
+            await window.navigator.clipboard.writeText(e.currentTarget.dataset.copy);
+            e.currentTarget.classList.remove('toCopy');
+            e.currentTarget.classList.add('toCheck');
+            setTimeout(() => {
+                e.currentTarget.classList.remove('toCheck');
+                e.currentTarget.classList.add('toCopy');
+            }, 2000);
+        });
+    </script>
 @endsection
