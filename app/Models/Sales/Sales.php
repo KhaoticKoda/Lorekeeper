3b5fd08ebb93d21478ec1231dd994e7a26ca19a4
SpeedyD <speedydvv@gmail.com>
2025-04-19 21:47:18 +0200

    optimization(sales): Replaced OldestFirst with NewestFirst reversed

--- app/Models/Sales/Sales.php
+++ app/Models/Sales/Sales.php
@@ -138,19 +138,8 @@ class Sales extends Model implements Feedable {
      *
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function scopeSortNewest($query) {
-        return $query->orderBy('id', 'DESC');
-    }
-
-    /**
-     * Scope a query to sort sales oldest first.
-     *
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function scopeSortOldest($query) {
-        return $query->orderBy('id');
+    public function scopeSortNewest($query, $reverse = false) {
+        return $query->orderBy('id', $reverse ? 'ASC' : 'DESC');
     }
 
     /**
