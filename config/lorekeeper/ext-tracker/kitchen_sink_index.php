90c71a32a9c209b34a721c1c15349851143cce8f
SpeedyD <speedydvv@gmail.com>
2025-05-26 22:20:41 +0200

    Restoring v3 Visible functions and adding ext tracker

---
+++ config/lorekeeper/ext-tracker/kitchen_sink_index.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'Kitchen_Sink_Index',
+    'creators' => json_encode([
+        'Speedy' => 'https://github.com/SpeedyD/',
+    ]),
+    'version'  => '1.0.0',
+];
