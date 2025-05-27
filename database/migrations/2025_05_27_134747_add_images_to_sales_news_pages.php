30ffd68110884295bf2c21d688d7f41f5c6482f1
SpeedyD <speedydvv@gmail.com>
2025-05-27 17:35:55 +0200

    I have only one question and it is "HOW"

--- database/migrations/2025_05_27_134747_add_images_to_sales_news_pages.php
+++ database/migrations/2025_05_27_134747_add_images_to_sales_news_pages.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class AddImagesToSalesNewsPages extends Migration {
-{
     /**
      * Run the migrations.
      */
