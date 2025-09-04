df80a79106a03a0d12eab07df42fae87435ab211
Alliionart <Alliionart@users.noreply.github.com>
2025-09-04 22:34:34 +0000

    refactor: fix PHP styling

--- app/Http/Controllers/FileManagerController.php
+++ app/Http/Controllers/FileManagerController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 
 class FileManagerController extends Controller {
     public function index() {
-        if( !Auth::check() || !Auth::user()->isStaff || !Auth::user()->hasPower('manage_files') ) {
+        if (!Auth::check() || !Auth::user()->isStaff || !Auth::user()->hasPower('manage_files')) {
             abort(404);
         }
+
         return view('filemanager');
     }
 }
