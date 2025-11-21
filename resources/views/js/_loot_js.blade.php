6f0bbde36e489d262a09e59b6a35606ad16deeec
TOYHOUSE_GAMING\otter <ottersir@gmail.com>
2025-11-20 19:41:49 -0800

    rewards js fix

--- resources/views/js/_loot_js.blade.php
+++ resources/views/js/_loot_js.blade.php
@@ -1,26 +1,11 @@
 <script>
-$( document ).ready(function() {    
-    var $lootTable  = $('#lootTableBody');
-    var $lootRow = $('#lootRow').find('.loot-row');
-    var $itemSelect = $('#lootRowData').find('.item-select');
-    var $currencySelect = $('#lootRowData').find('.currency-select');
-    var $awardSelect = $('#lootRowData').find('.award-select');
-    @if($showLootTables)
-        var $tableSelect = $('#lootRowData').find('.table-select');
-    @endif
-
-    });
-
-    $('.reward-type').on('change', function(e) {
-        var val = $(this).val();
-        var $cell = $(this).parent().find('.loot-row-select');
-
-        var $clone = null;
-        if(val == 'Item') $clone = $itemSelect.clone();
-        else if (val == 'Currency') $clone = $currencySelect.clone();
-        else if (val == 'Award') $clone = $awardSelect.clone();
-        @if($showLootTables)
-            else if (val == 'LootTable') $clone = $tableSelect.clone();
+    $(document).ready(function() {
+        var $lootTable = $('#lootTableBody');
+        var $lootRow = $('#lootRow').find('.loot-row');
+        var $itemSelect = $('#lootRowData').find('.item-select');
+        var $currencySelect = $('#lootRowData').find('.currency-select');
+        @if ($showLootTables)
+            var $tableSelect = $('#lootRowData').find('.table-select');
         @endif
         @if ($showRaffles)
             var $raffleSelect = $('#lootRowData').find('.raffle-select');
@@ -35,6 +20,25 @@ $( document ).ready(function() {
             $lootTable.append($clone);
             attachRewardTypeListener($clone.find('.reward-type'));
             attachRemoveListener($clone.find('.remove-loot-button'));
+        });
+
+        $('.reward-type').on('change', function(e) {
+            var val = $(this).val();
+            var $cell = $(this).parent().parent().find('.loot-row-select');
+
+            var $clone = null;
+            if (val == 'Item') $clone = $itemSelect.clone();
+            else if (val == 'Currency') $clone = $currencySelect.clone();
+            else if (val == 'Award') $clone = $awardSelect.clone();
+            @if ($showLootTables)
+                else if (val == 'LootTable') $clone = $tableSelect.clone();
+            @endif
+            @if ($showRaffles)
+                else if (val == 'Raffle') $clone = $raffleSelect.clone();
+            @endif
+
+            $cell.html('');
+            $cell.append($clone);
         });
 
         function attachRewardTypeListener(node) {
@@ -56,7 +60,7 @@ $( document ).ready(function() {
                 $cell.append($clone);
                 $clone.selectize();
             });
-        };
+        }
 
         function attachRemoveListener(node) {
             node.on('click', function(e) {
